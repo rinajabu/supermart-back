675a83d00111f3ca7ac469cebaa8754a851ab772
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-08-29 19:07:42 -0400

    products crud complete

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $newProduct = ["category" => $input["category"], "name" => $input["name"], "price" => $input["price"], "description" => $input["description"], "picture" => $input["picture"]];
+        $product = Product::create($newProduct);
+        response()->json(Product::all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+        $updatedProduct = ["category" => $input["category"], "name" => $input["name"], "price" => $input["price"], "description" => $input["description"], "picture" => $input["picture"]];
+        $product = Product::find($id)->update($updatedProduct);
+        response()->json(Product::all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Product::find($id)->delete();
+        response()->json(Product::all());
+    }
+}
