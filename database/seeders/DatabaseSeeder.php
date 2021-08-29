1ecc78ae7d06846e5af416c26ee9226463b7c1b4
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-08-28 21:10:09 -0400

    products schema and model done; seeded with products and one user testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Product::create(["category" => "Meat", "name" => "Chicken Wings", "price" => 12, "description" => "chicken wings from a chicken", "picture" => "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00270874000004/102a1da685c6bda2eb17d7cc9999359c_large.png&width=512&type=webp&quality=80"]);
+        // Product::create(["category" => "Dairy", "name" => "Milk", "price" => 3, "description" => "milk from a cow", "picture" => "https://i5.walmartimages.com/asr/3592de4c-2d2d-4285-afbf-f0508775bd58_2.bb23225176016b4d5ce96c4efed80382.jpeg"]);
+        // Product::create(["category" => "Fruit", "name" => "Red Delicious Apples", "price" => 1, "description" => "apple from a tree", "picture" => "https://images.heb.com/is/image/HEBGrocery/000325189"]);
+
+        // User::create(["name" => "testing", "email" => "testing@testing.com", "password" => "testing"]);
     }
 }
