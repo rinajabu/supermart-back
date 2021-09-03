2fd5b0623ea1fef91592fcf1c6fd42efef05ee9f
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-03 11:14:58 -0400

    auth not working properly; can get cookie but cant post login

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::apiResource("products", ProductController::class);
